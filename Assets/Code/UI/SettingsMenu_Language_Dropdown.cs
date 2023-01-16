4449de4cd4080551ba7248e430faa4a6e90367d6
bulteir <talha_alpay20@hotmail.com>
2023-01-16 14:19:52 +0300

    -toplam 10 dile çeviri yapıldı. -dillerle alakalı font eksiklikleri giderildi. -geri butonlarına safe area özelliği eklendi.

--- Assets/Code/UI/SettingsMenu_Language_Dropdown.cs
+++ Assets/Code/UI/SettingsMenu_Language_Dropdown.cs
@@ -20,6 +20,46 @@ public class SettingsMenu_Language_Dropdown : MonoBehaviour
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
             PlayerPrefs.SetString("SelectedLang", "1");
         }
+        else if (selectedItemVal == 2)//çince
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[2];
+            PlayerPrefs.SetString("SelectedLang", "2");
+        }
+        else if (selectedItemVal == 3)//arapça
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[3];
+            PlayerPrefs.SetString("SelectedLang", "3");
+        }
+        else if (selectedItemVal == 4)//fransýzca
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[4];
+            PlayerPrefs.SetString("SelectedLang", "4");
+        }
+        else if (selectedItemVal == 5)//almanca
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[5];
+            PlayerPrefs.SetString("SelectedLang", "5");
+        }
+        else if (selectedItemVal == 6)//japonca
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[6];
+            PlayerPrefs.SetString("SelectedLang", "6");
+        }
+        else if (selectedItemVal == 7)//portekizce
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[7];
+            PlayerPrefs.SetString("SelectedLang", "7");
+        }
+        else if (selectedItemVal == 8)//rusça
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[8];
+            PlayerPrefs.SetString("SelectedLang", "8");
+        }
+        else if (selectedItemVal == 9)//ispanyolca
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[9];
+            PlayerPrefs.SetString("SelectedLang", "9");
+        }
         PlayerPrefs.Save();
     }
 }
